a2dca777148caa89b798cea9b416cbde40c98dce
AkshatJindal1 <ajindal.iitkgp@gmail.com>
2025-09-28 22:28:19 +0530

    Add: Error snackbar for displaying errors

--- src/components/ErrorSnackbar.jsx
+++ src/components/ErrorSnackbar.jsx
@@ -14,7 +14,7 @@ export const ErrorSnackbar = ({ message, onClear }) => {
     <div
       id="error-snackbar"
       className={`fixed bottom-4 left-1/2 -translate-x-1/2 bg-red-600 text-white dark:bg-red-500 dark:text-white px-6 py-3 rounded-lg shadow-lg transition-transform duration-300 ${
-        message ? 'visible' : ''
+        message ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
       }`}
     >
       {message}
